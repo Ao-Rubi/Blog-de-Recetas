b4f4e58a0ec0adf7c834aa07d9858408b91147d5
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-09-20 23:17:45 -0300

    Agregado rutas de administrar, crear y editar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Menu from './components/common/Menu';
 import Footer from "./components/common/Footer";
 import Home from './components/Views/Home';
-
+import AdministrarRecetas from './components/Views/recetas/AdministrarRecetas';
+import Error404 from "./components/Views/Error404"
+import CrearReceta from "./components/Views/recetas/CrearReceta"
+import EditarReceta from './components/Views/recetas/EditarReceta';
 
 function App() {
   return (
@@ -14,7 +17,11 @@ function App() {
         
         <Routes>
           <Route exact path="/" element={<Home></Home>}></Route>
-          
+          <Route exact path="/administrar" element={<AdministrarRecetas></AdministrarRecetas>}></Route>
+          <Route exact path="/administrar/crear" element={<CrearReceta></CrearReceta>}></Route>
+          <Route exact path="/administrar/editar/:id" element={<EditarReceta></EditarReceta>}></Route>
+
+          <Route path="*" element={<Error404></Error404>}></Route>
         </Routes>
 
         <Footer></Footer>
