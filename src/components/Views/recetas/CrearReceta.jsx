5c08d02807fc25ecc81e771131b56dbea5c95306
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-09-20 23:18:27 -0300

    Agregado de componentes, modificar a gusto

---
+++ src/components/Views/recetas/CrearReceta.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CrearReceta = () => {
+    return (
+        <div>
+            Pagina de crear receta
+        </div>
+    );
+};
+
+export default CrearReceta;
