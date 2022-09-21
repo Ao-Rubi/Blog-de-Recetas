20b51f041ea3ecccb5df620a27c05dbda52e51c5
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-09-21 18:39:33 -0300

    Agregado la variable de entorno y el funcionamiento de creacion de receta

--- src/components/Views/recetas/CrearReceta.jsx
+++ src/components/Views/recetas/CrearReceta.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
 const CrearReceta = () => {
 
@@ -9,12 +11,61 @@ const CrearReceta = () => {
     const [imagen, setImagen] = useState("")
     const [categoria, setCategoria] = useState("")
 
+    //Variable de entorno
+    const URL = process.env.REACT_APP_API_SERVER;
+
+    // Inicializar useNavigate
+    const navegacion = useNavigate();
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+
+        //Validaciones
+        //creo el objeto
+        const nuevaReceta = {
+            titulo: titulo,
+            descripcion: descripcion,
+            imagen: imagen,
+            categoria: categoria
+        }
+
+        try {
+            // envio peticion del objeto a la API
+            const respuesta = await fetch(URL, {
+                method: "POST",
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify(nuevaReceta)
+            })
+
+            if (respuesta.status === 201) {
+                
+                // Mensaje que todo salio bien
+                Swal.fire(
+                    'Receta creado!',
+                    'La receta fue creada correctamente',
+                    'success'
+                )
+
+                // Redirecciono a la pagina de administrar
+                navegacion("/administrar")
+            }
+
+            console.log(respuesta)
+
+        } catch (error) {
+            console.log(error)
+            //mensaje de error
+        }
+    }
+
     return (
         <section className='container'>
             <h1 className='display-4 mt-5'>Nueva receta</h1>
             <hr />
 
-            <Form >
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="titulo">
                     <Form.Label>Titulo</Form.Label>
                     <Form.Control onChange={(e)=>{setTitulo(e.target.value)}} type="text" placeholder="Ej: Yogurt griego" />
@@ -22,7 +73,7 @@ const CrearReceta = () => {
 
                 <Form.Group className="mb-3" controlId="descripcion">
                     <Form.Label>Descripcion</Form.Label>
-                    <Form.Control onChange={(e)=>{setDescripcion(e.target.value)}} type="number" as="textarea" placeholder="Pasos a seguir de la receta"/>
+                    <Form.Control onChange={(e)=>{setDescripcion(e.target.value)}} type="text" as="textarea" placeholder="Pasos a seguir de la receta"/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="imagen">
@@ -32,10 +83,10 @@ const CrearReceta = () => {
 
                 <Form.Group className="mb-3" controlId="categoria">
                     <Form.Label>Categoria</Form.Label>
-                    <Form.Control onChange={(e)=>{setCategoria(e.target.value)}} type="number" placeholder="Dulce, salado, etc" />
+                    <Form.Control onChange={(e)=>{setCategoria(e.target.value)}} type="text" placeholder="Dulce, salado, etc" />
                 </Form.Group>
 
-                <Button className='ms-auto' variant="primary" type="submit">
+                <Button className='ms-auto mb-1' variant="primary" type="submit">
                     Submit
                 </Button>
             </Form>
