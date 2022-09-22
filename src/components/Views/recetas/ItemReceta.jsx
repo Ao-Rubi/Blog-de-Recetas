285a0891154ad9e5f9629f7edd450203efd60ddb
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-09-21 21:02:37 -0300

    Funcionamiento del boton de borrar, y la info mostrada en el componente

--- src/components/Views/recetas/ItemReceta.jsx
+++ src/components/Views/recetas/ItemReceta.jsx
@@ -1,18 +1,60 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
-const ItemReceta = () => {
+const ItemReceta = ({receta, consultarAPI}) => {
+    const {_id, titulo, categoria, descripcion, imagen} = {...receta}
+    const URL = process.env.REACT_APP_API_SERVER;
+
+    const handleDelete = ()=>{
+        Swal.fire({
+            title: 'Seguro de borrar?',
+            text: "No hay vuelta atras",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Borrar!',
+            cancelButtonText: "Cancelar",
+        }).then(async (result)=>{
+            if (result.isConfirmed) {
+                //realizo peticion para borrar
+                try {
+                    const parametro = {
+                        method: "DELETE"
+                    }
+                    const respuesta = await fetch(URL + "/" + _id, parametro)
+                    console.log(respuesta)
+
+                    if (respuesta.status === 200) {
+
+                        Swal.fire(
+                            'Receta eliminado!',
+                            'La receta fue eliminada correctamente.',
+                            'success'
+                        )
+                        // Recargar la tabla de productos
+                        consultarAPI();
+                    }
+
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        })
+    }
+
     return (
         <tr>
-            <td>123</td>
-            <td>Magdalenas</td>
-            <td>Dulce</td>
-            <td className='truncate'>Bolitas con relleno de dulce de leche</td>
-            <td className='truncate'>https://images.hola.com/imagenes/cocina/recetas/20220314206228/magdalenas-caseras/1-62-782/magdalenas-age-m.jpg</td>
+            <td>{_id}</td>
+            <td>{titulo}</td>
+            <td>{categoria}</td>
+            <td className='truncate'>{descripcion}</td>
+            <td className='truncate'>{imagen}</td>
             <td className='d-block'> 
-                <Link className='btn btn-warning' to="/administrar/editar/:id">Editar</Link>
-                <Button className='btn btn-danger'>Borrar</Button>
+                <Link className='btn btn-warning' to={`/administrar/editar/${_id}`}>Editar</Link>
+                <Button className='btn btn-danger' onClick={handleDelete}>Borrar</Button>
             </td>
         </tr>
     );
